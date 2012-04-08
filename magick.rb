f2c9c601f83251cff17db97135a823ab9ed78a35
Roma Sharma <romaspace@gmail.com>
2012-04-09 01:08:28 +0530

    updated

--- magick.rb
+++ magick.rb
@@ -9,14 +9,20 @@ data = File.read(ARGV[0] || 'book.text')
 def create_toc_html(toc) 
   file = File.open('toc.html','w')
   file.puts("<h1><a name='TOC'><center>TABLE OF CONTENTS</center></a></h1>")
-  h1_count = h2_count = 1
+  h1_count = h2_count = recipe_count = 1
   toc.each do |h1|
     file.puts("<div class='toc_h1'><a href='#ch_h1_#{h1_count}'>#{h1[:title]}</a></div>")
     h1_count += 1
+    is_recipe = h1[:title] =~ /phase/i
     if h1[:subheading].length > 0
       file.puts("<blockquote>")
       h1[:subheading].each do |h2|
-        file.puts("    <div class='toc_h2'><a href='#ch_#{h2_count}'>#{h2_count}. #{h2}</a></div>")
+        if (is_recipe)
+          file.puts("    <div class='toc_h2'><a href='#ch_#{h2_count}'>#{recipe_count}. #{h2}</a></div>")
+          recipe_count += 1
+        else
+          file.puts("    <div class='toc_h2'><a href='#ch_#{h2_count}'>#{h2}</a></div>")
+        end
         h2_count += 1
       end
       file.puts("</blockquote>")
@@ -29,9 +35,10 @@ def create_toc_ncx(toc)
   file = File.open('toc.ncx','w')
   file.puts("<ncx>")
   file.puts("<navMap>")
-  h1_count = h2_count = play_order = 1
+  h1_count = h2_count = play_order = recipe_count = 1
   toc.each do |h1|
     title = h1[:title].gsub(/<.*?>/,'')
+    is_recipe = h1[:title] =~ /phase/i
     file.puts <<EOF
   <navPoint class="titlepage" id="L#{h1_count}T" playOrder="#{play_order}">
     <navLabel><text>#{title}</text></navLabel>
@@ -41,9 +48,14 @@ EOF
     play_order += 1
    
     h1[:subheading].each do |h2|
+      _prefix = ""
+      if is_recipe
+        _prefix = "#{recipe_count}. "
+        recipe_count += 1
+      end
       file.puts <<EOF
     <navPoint class="chapter" id="level2-chap#{h2_count}" playOrder="#{play_order}">
-      <navLabel><text>#{h2_count}. #{h2}</text></navLabel>
+      <navLabel><text>#{_prefix}#{h2}</text></navLabel>
       <content src="book.html#ch_#{h2_count}" />
     </navPoint>
 EOF
@@ -58,8 +70,6 @@ EOF
 end
 
 html_data = RDiscount.new(data).to_html
-h2_count = 1
-h1_count = 1
 new_lines = []
 dir_exists = false
 if File.exists?("c")
@@ -84,22 +94,31 @@ end
 create_toc_html(toc)
 create_toc_ncx(toc)
 
+h1_count = h2_count = recipe_count = 1
+recipe_started = false
+
 html_data.lines.each do |l|
   if l =~ /<h1>(.*)<\/h1>/i
     _title = $~[1]
+    recipe_started = _title =~ /phase/i
     new_lines << "<h1><a name='ch_h1_#{h1_count}'>#{_title}</a></h1>"
     h1_count += 1
   elsif l =~ /<h2>(.*)<\/h2>/i
     _title = $~[1]
-    new_lines << "<h2><a name='ch_#{h2_count}'>#{h2_count}. #{_title}</a></h2>"
+    if recipe_started
+      new_lines << "<h2><a name='ch_#{h2_count}'>#{recipe_count}. #{_title}</a></h2>"
+      recipe_count += 1
+    else
+      new_lines << "<h2><a name='ch_#{h2_count}'>#{_title}</a></h2>"
+    end
     h2_count += 1
-  elsif l =~ /blockquote.*nutrition/
-    new_lines << "<h4>Nutritional Facts</h4>"
-    new_lines << l
-  elsif l =~ /<blockquote>/
-    l = l.sub(/<blockquote>/,"<blockquote class='info'>")
-    new_lines << "<h4>Please Note</h4>"
-    new_lines << l
+  #elsif l =~ /blockquote.*nutrition/
+  #  new_lines << "<h4>Nutritional Facts</h4>"
+  #  new_lines << l
+  #elsif l =~ /<blockquote>/
+  #  l = l.sub(/<blockquote>/,"<blockquote class='info'>")
+  #  new_lines << "<h4>Please Note</h4>"
+  #  new_lines << l
   else
     new_lines << l
   end
